cef21f6827ed7e286e5c938687cb2d06ec1ab3ae
Jhonatan AG <jhonatanaguilera334@gmail.com>
2024-02-09 17:22:20 -0600

    Modulo de busqueda y header cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/home/components/searcher/searcher.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-searcher',
+  templateUrl: './searcher.component.html',
+  styleUrls: ['./searcher.component.scss']
+})
+export class SearcherComponent {
+
+}
