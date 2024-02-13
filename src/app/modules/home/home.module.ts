e732423b84244f7763a24a513fe5ed793a8df6de
Jhonatan AG <jhonatanaguilera334@gmail.com>
2024-02-13 16:48:12 -0600

    Travel finalizado y componente blog creado

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,6 +5,7 @@ import { MainComponent } from './components/main/main.component';
 import { MostSearchedComponent } from './components/most-searched/most-searched.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { TravelComponent } from './components/travel/travel.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 
 
@@ -13,7 +14,8 @@ import { TravelComponent } from './components/travel/travel.component';
     SearcherComponent,
     MainComponent,
     MostSearchedComponent,
-    TravelComponent
+    TravelComponent,
+    BlogComponent
   ],
   imports: [
     CommonModule,
