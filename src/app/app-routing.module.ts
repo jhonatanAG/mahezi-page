3adeb73ae1305364c78fbc68de32477a7056ff31
Jhonatan AG <jhonatanaguilera334@gmail.com>
2024-02-22 11:31:22 -0600

    Avance login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './components/main/main.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: MainComponent },
+];
 
 @NgModule({
   declarations: [
     MainComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class LoginModule { }
